eaa0971c2c2feda54000ce132b3140c905e6103d
Vladislav <vladdex@mail.ru>
2018-04-23 00:50:03 +0300

    redux support is added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+// Redux
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import reducers from './reducers';
+
+import './index.css';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-const store = createStore();
+
+const store = createStore(reducers);
 
 ReactDOM.render(
     <Provider store={store}>
