cdf375e52eaceb8310d80db047f2e92b3a141d9b
Vladislav <vladdex@mail.ru>
2018-04-23 00:25:43 +0300

    init project

---
+++ src/tickets-area/tickets-area.component.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TicketsArea extends Component {
+    state = {}
+    render() { 
+        return (
+            <div className="tickets-area">
+                
+            </div>
+        )
+    }
+}
+ 
+export default TicketsArea;
